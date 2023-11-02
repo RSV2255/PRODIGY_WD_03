896ba734d237eb4da859862d059a54cfe22e33bc
RSV2255 <rushivutukuri@gmail.com>
2023-11-02 19:48:51 +0530

    task 3

---
+++ asserts/script.js
@@ -0,0 +1,7 @@
+const result = document.getElementsByClassName("result-view")[0];
+
+const X_wins = "&#129321 X wins &#129321";
+const O_wins = "&#129321 O wins &#129321";
+const tie = "&#128521 Tie &#128521";
+
+result.innerHTML = tie;
