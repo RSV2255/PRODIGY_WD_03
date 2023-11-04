a95c50e8d906d9ae7ff3b329762c0d3fd4dcb5c6
RSV2255 <rushivutukuri@gmail.com>
2023-11-04 12:24:39 +0530

    task 3

--- asserts/script.js
+++ asserts/script.js
@@ -1,7 +1,43 @@
-const result = document.getElementsByClassName("result-view")[0];
+const mainPage = document.getElementsByClassName("mainpage")[0];
+const gamingPage = document.getElementsByClassName("game")[0];
+const resultPage = document.getElementsByClassName("result")[0];
 
-const X_wins = "&#129321 X wins &#129321";
-const O_wins = "&#129321 O wins &#129321";
-const tie = "&#128521 Tie &#128521";
+const singlePlay = document.getElementsByClassName("singleplay")[0];
+const multiPlay = document.getElementsByClassName("multiplay")[0];
+const restart = document.getElementsByClassName("restart")[0];
+const exit = document.getElementsByClassName("exit")[0];
+const playAgain = document.getElementsByClassName("playAgain")[0];
+const newGame = document.getElementsByClassName("Newgame")[0];
 
-result.innerHTML = tie;
+const playCheck = false;
+
+const openCheck = [mainPage,gamingPage,resultPage];
+const view = (page) => {
+    for(let i=0;i<openCheck.length;i++) {
+        if(openCheck[i] === page){
+            openCheck[i].classList.remove("hide");
+        }
+        else {
+            openCheck[i].classList.add("hide");
+        }
+    }
+}
+view(resultPage);
+
+const play = (isSinglePLay) => {
+    view(gamingPage);
+    playCheck = isSinglePLay;
+}
+
+const exitToMainPage = () => {
+    view(mainPage);
+}
+
+const continueTogamePage = () => {
+    view(gamingPage);
+}
+
+singlePlay.addEventListener('click',play(true));
+multiPlay.addEventListener('click',play(false));
+exit.addEventListener('click',exitToMainPage);
+newGame.addEventListener('click',exitToMainPage);
