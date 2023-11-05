e17db3a85f0ad7c6942829f432b98e613c0f6e6b
RSV2255 <rushivutukuri@gmail.com>
2023-11-05 16:12:18 +0530

    task 3

--- asserts/script.js
+++ asserts/script.js
@@ -1,43 +1,32 @@
+// pages
 const mainPage = document.getElementsByClassName("mainpage")[0];
+const choosingMark = document.getElementsByClassName("playersmark")[0];
 const gamingPage = document.getElementsByClassName("game")[0];
 const resultPage = document.getElementsByClassName("result")[0];
 
-const singlePlay = document.getElementsByClassName("singleplay")[0];
-const multiPlay = document.getElementsByClassName("multiplay")[0];
-const restart = document.getElementsByClassName("restart")[0];
+mainPage.classList.add("hide");
+choosingMark.classList.add("hide");
+gamingPage.classList.add("hide");
+resultPage.classList.add("hide");
+
+
+// buttons
+const singleplayerButton = document.getElementsByClassName("singleplay")[0];
+const multiplayerButton = document.getElementsByClassName("multiplay")[0];
+
+const pickX = document.getElementsByClassName("pick_X")[0];
+const pickO = document.getElementsByClassName("pick_O")[0];
+
+const startGameButton = document.getElementsByClassName("startGame")[0];
+const backButton = document.getElementsByClassName("back")[0];
+
+const box = document.getElementsByClassName("square");
+const restartButton = document.getElementsByClassName("restart")[0];
 const exit = document.getElementsByClassName("exit")[0];
-const playAgain = document.getElementsByClassName("playAgain")[0];
-const newGame = document.getElementsByClassName("Newgame")[0];
 
-const playCheck = false;
+const playAgainButton = document.getElementsByClassName("playAgain")[0];
+const newGameButton = document.getElementsByClassName("Newgame")[0];
 
-const openCheck = [mainPage,gamingPage,resultPage];
-const view = (page) => {
-    for(let i=0;i<openCheck.length;i++) {
-        if(openCheck[i] === page){
-            openCheck[i].classList.remove("hide");
-        }
-        else {
-            openCheck[i].classList.add("hide");
-        }
-    }
-}
-view(resultPage);
-
-const play = (isSinglePLay) => {
-    view(gamingPage);
-    playCheck = isSinglePLay;
-}
-
-const exitToMainPage = () => {
-    view(mainPage);
-}
-
-const continueTogamePage = () => {
-    view(gamingPage);
-}
-
-singlePlay.addEventListener('click',play(true));
-multiPlay.addEventListener('click',play(false));
-exit.addEventListener('click',exitToMainPage);
-newGame.addEventListener('click',exitToMainPage);
+const xScore = document.getElementsByClassName("X_score")[0];
+const oScore = document.getElementsByClassName("O_score")[0];
+const tieScore = document.getElementsByClassName("tie_score")[0];
