6b2393fce0113b14636a62c38871282f0b655493
RSV2255 <rushivutukuri@gmail.com>
2023-11-07 00:58:18 +0530

    task 3

--- asserts/script.js
+++ asserts/script.js
@@ -1,4 +1,4 @@
-// pages
+
 const mainPage = document.getElementsByClassName("mainpage")[0];
 const choosingMark = document.getElementsByClassName("playersmark")[0];
 const gamingPage = document.getElementsByClassName("game")[0];
@@ -8,7 +8,7 @@ const resultPage = document.getElementsByClassName("result")[0];
 
 
 
-// buttons
+
 const singleplayerButton = document.getElementsByClassName("singleplay")[0];
 const multiplayerButton = document.getElementsByClassName("multiplay")[0];
 
@@ -49,7 +49,15 @@ let countMarked = 0;
 let xWins = 0;
 let oWins = 0;
 let tie = 0;
-let isHuman = false;
+let isHuman = true;
+let ai;
+let human;
+let winner = null;
+const scores = {
+    'X':1,
+    'O':-1,
+    'tie':0
+}
 const view = (page) => {
     for (let x of pages) {
         if(x === page) {
@@ -62,6 +70,17 @@ const view = (page) => {
 }
 
 const updateScoreBoard = () => {
+    switch(winner) {
+        case 'X':
+            xWins++;
+            break;
+        case 'O':
+            oWins++;
+            break;
+        case 'tie':
+            tie++;
+            break;
+    }
     xScore.innerHTML = xWins;
     oScore.innerHTML = oWins;
     tieScore.innerHTML = tie;
@@ -76,47 +95,49 @@ const resetGameBoard = () => {
     tie = 0;
 }
 
-const isGameOver = (x) => {
+const isGameOver = () => {
+    winner = null;
     let isWin = false;
         let checkWin = ['','',''];
         for (let pattern of possibleWins) {
-            if (pattern[0] === x ||pattern[1] === x || pattern[2] === x ) {
+             
                 for (let i = 0; i < pattern.length; i++) {
                     checkWin[i] = values[pattern[i]];
                 }
-            
                 if (checkWin[0] !== '' || checkWin[1] !== '' || checkWin[2] !== '') {
                     if (checkWin[0] === checkWin[1] && checkWin[1] === checkWin[2]) {
                         view(resultPage);
                         gamingPage.classList.remove("hide");
-                        if (values[x] === 'X') {
+                        isWin = true;
+                        if (checkWin[0] === 'X') {
+                            winner = 'X';
                             resultDisplay.innerHTML = "&#127881 X wins &#127881";
-                            xWins++;
-                            
+                            break;
                         }
                         else {
+                            winner = 'O';
                             resultDisplay.innerHTML = "&#127881 O wins &#127881";
-                            oWins++;
-                            
+                            break;
                         }
-                        isWin = true;
                     }
                     else {
-                        if (countMarked ===  9 && !isWin) {
+                        if (countMarked ===  9) {
+                            winner = 'tie';
                             view(resultPage);
                             gamingPage.classList.remove("hide");
                             resultDisplay.innerHTML = "&#127881 Tie &#127881";
-                            tie++;
                             isWin = true;
                         }
                     }
+                    
                 }
-                
-            }
+                else {
+                    break;
+                }
             
         }
-        console.clear();
-        console.log(`X : ${xWins} => O : ${oWins} => tie : ${tie}`);
+    
+    updateScoreBoard();
     return isWin;
 }
 
@@ -136,12 +157,24 @@ const multiPlayer = () => {
                 values[i] = 'O';
             }
             countMarked++;
-            isGameOver(i);
+            isGameOver();
+            
            }
         });
     }
 }
-
+const xPicked = () => {
+    isHuman = true;
+    human = 'X';
+    ai = 'O';
+    singlePlayer();
+}
+const oPicked = () => {
+    isHuman = false;
+    human = 'O';
+    ai = 'X';
+    singlePlayer();
+}
 const availableSpots = (filled) => {
     let available = [];
     for (let x = 0; x < filled.length; x++) {
@@ -155,57 +188,116 @@ const availableSpots = (filled) => {
 const marking = (i,value) => {
     box[i].classList.add(value);
     values[i] = value;
-} 
-
-const nextMove = () => {
-    let available = availableSpots(values);
-    let i = available[Math.floor((Math.random() * available.length))];
-    if (xTurn) {
-        marking(i,'X');
-        xTurn = false;
+    countMarked++;
+}
+const isWinner = () => {
+    winner = null;
+        let checkWin = ['','',''];
+        for (let pattern of possibleWins) {
+                for (let i = 0; i < pattern.length; i++) {
+                    checkWin[i] = values[pattern[i]];
+                }
+            if (checkWin[0] !== '' && checkWin[1] !== '' && checkWin[2] !== '') {
+                if (checkWin[0] === checkWin[1] && checkWin[1] === checkWin[2]) {
+                    
+                    if (checkWin[0] === 'X') {
+                        winner = 'X';
+                    }
+                    else {
+                        winner = 'O';
+                    }
+                }
+                else {
+                    if (countMarked ===  9) {
+                        winner = 'tie';      
+                    }
+                }
+            }
+        }
+        return winner;
+}
+const aiMove = (values,isAi,depth,spot) => {
+    const emptyBoxes = availableSpots(values);
+    if((spot !== null ))  {
+        let score = scores[spot];
+        return score;
+    }
+    if(isAi) {
+        let spot = isWinner();
+        let maxEval = -Infinity;
+        for(let x of emptyBoxes) {
+            values[x] = ai;
+            let eval = aiMove(values,false,depth--,spot);
+            values[x] = '';
+            if(eval > maxEval) {
+                maxEval = eval;
+            }
+        }
+        return maxEval;
     }
     else {
-        marking(i,'O');
-        xTurn = true;
+        let spot = isWinner();
+        let minEval = Infinity;
+        for(let x of emptyBoxes) {
+            values[x] = human;
+            
+            let eval = aiMove(values,true,depth--, spot);
+            values[x] = '';
+            if(eval < minEval) {
+                minEval = eval;
+            }
+        }
+        return minEval;
     }
-    countMarked++;
-    isGameOver(i);
+}
+
+const firstMove = () => {
+    let i;
+    let available = availableSpots(values);
+    if (countMarked < 0) {
+        i = available[Math.floor((Math.random() * available.length))];
+    }
+    else {
+        // i = available[0];
+        let spot = isWinner();
+        let maxEval = -Infinity;
+        for(let x of available) {
+            values[x] = ai;
+            let eval = aiMove(values,false,countMarked++,spot);
+            values[x] = '';
+            if(eval > maxEval) {
+                maxEval = eval;
+                i = x;
+            }
+        }
+    }
+    marking(i,ai);
+    isGameOver();
 }
 
 const singlePlayer = () => {
-    
     view(gamingPage);
     if (!isHuman) {
-        nextMove();
+        firstMove();
         isHuman = true;
-        xTurn = false;
     }
     if (isHuman) {
         for (let i = 0; i < box.length; i++) {
             box[i].addEventListener('click', () => {
                if (values[i] === '') {
-                if (xTurn) {
-                    marking(i,'X');
-                    xTurn = false;
-                }
-                else {
-                    marking(i,'O');
-                    xTurn = true;
-                }
-                countMarked++;
-                isGameOver(i);
-                if (!isGameOver(i)) {
-                    nextMove();
-                }
-                
+                    marking(i,human);
+                    if(!isGameOver()) {
+                        firstMove();
+                    }
                }
             });
         }
     }
 }
-
+pickO.addEventListener('click',oPicked);
+pickX.addEventListener('click',xPicked);
+singleplayerButton.addEventListener('click',()=> {view(choosingMark);});
 multiplayerButton.addEventListener('click',multiPlayer);
-singlePlayer();
 
 
 
